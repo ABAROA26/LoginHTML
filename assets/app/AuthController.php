23e173b49c7c47e41d05ea4de6488733cb6a6468
ABAROA26 <diegoemiln_23@alu.uabcs.mx>
2025-10-24 19:20:27 -0700

    "Login"

---
+++ assets/app/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+$username = $_POST['username'] ?? '';
+$password = $_POST['password'] ?? '';
+
+
+if ($username == 'diego') {
+    
+    if ($password == 'vildosola') {
+
+        
+        header("Location: ../../home.html");
+        exit();
+    } 
+}else 
+    header("Location: ../../index.html");
+    exit();
+
+    
+?>
